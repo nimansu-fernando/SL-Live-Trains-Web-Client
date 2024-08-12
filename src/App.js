05e76d7168dcc63a7106ac76484f57ec4844e4a4
nimansu-fernando <nimansufernando@gmail.com>
2024-08-12 18:36:26 +0530

    Add Home page and integrate TrainCard component within it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/header.js';
+import Home from './pages/home.js';
 import Footer from './components/footer.js';
 
 function App() {
   return (
     <div className="App">
       <Header />
+        <Home />
       <Footer/>
     </div>
   );
---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TrainCard from '../components/trainCard.js';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <TrainCard />
+    </div>
+  );
+};
+
+export default Home;
