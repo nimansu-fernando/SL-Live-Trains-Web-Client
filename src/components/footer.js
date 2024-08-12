8f870f2b853e193215187d43dc895b63a4e2428a
nimansu-fernando <nimansufernando@gmail.com>
2024-08-12 13:06:21 +0530

    Added SL Railways and SL Emblem logos to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import '../styles/footer.css';
+import SlEmblem from '../assets/EmblemSL.png';
+import RailwayLogo from '../assets/Sri_Lanka_Railway_logo.png';
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="container">
         <div className="footer-content">
-          <p>© 2022 Sri Lanka Railways (SLR). All rights Reserved Sri Lanka Railways</p>
+          <div className="footer-top">
+            <p>© 2022 Sri Lanka Railways (SLR). All rights Reserved Sri Lanka Railways</p>
+            <div className="footer-logos">
+              <img src={SlEmblem} alt="SL Emblem" className="footer-logo" />
+              <div className="separator"></div>
+              <img src={RailwayLogo} alt="SLR Logo" className="footer-logo" />
+            </div>
+          </div>
           <address>
             Sri Lanka Railways<br />
             Sri Lanka Railways Headquarters,<br />
             Colombo 10, Sri Lanka<br />
-            Telephones: +94 11 4 600 111
+            Telephone: +94 11 4 600 111
           </address>
-          <div className="footer-links">
-            <a href="#faq">FAQ</a>
-            <a href="#about-us">About us</a>
-            <a href="#privacy-policy">Privacy policy</a>
-            <a href="#train-timetable">Train timetable</a>
-          </div>
+        <div className="footer-links">
+          <a href="#faq">FAQ</a>
+          <a href="#about-us">About us</a>
+          <a href="#privacy-policy">Privacy policy</a>
+          <a href="#train-timetable">Train timetable</a>
+        </div>
         </div>
       </div>
     </footer>
