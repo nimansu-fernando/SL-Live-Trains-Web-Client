69e4e902b38e7e668ad8cf616cc94d764d05f497
nimansu-fernando <nimansufernando@gmail.com>
2024-08-13 07:08:46 +0530

    Update App.js, Home.js, and TrainCard.js: refactor layout, add new features and improvements

--- src/components/trainCard.js
+++ src/components/trainCard.js
@@ -1,10 +1,66 @@
-import React, { useState } from 'react';
+import React,{useState} from 'react';
+import trainIcon from '../assets/train-icon.png';
+import '../styles/trainCard.css'
 
-const TrainCard = () => {
+const TrainCard = ({train}) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleStations = () => {
+    setIsExpanded(!isExpanded);
+  };
 
   return (
     <div className="train-card">
-        
+        <div className="train-icon">
+            <img src={trainIcon} alt="Train" />
+        </div>
+        <div className="train-info">
+            <div className="train-header">
+                <div className="train-time">
+                    <span className="start-time">{train.startTime}</span>
+                    <span className="route">{train.route}</span>
+                    <span className="train-name">({train.trainName})</span>
+                </div>
+            </div>
+            <div className="train-details">
+                <span className="days">{train.days}</span>
+                <span className="days">{train.trainNo} - {train.type}</span>
+            </div>
+            <div className="train-details">
+                <span className="train-startEnd">{train.startTime}</span>                
+                <span className="train-startEnd">● ············································································································································································································· ●</span>
+                <span className="train-startEnd">{train.endTime}</span>
+            </div>
+            <div className="train-details">
+                <span className="train-startEnd">{train.startStation}</span>
+                <span className="duration">{train.duration}</span>
+                <span className="train-startEnd">{train.endStation}</span>
+            </div>
+            <div className="train-details">
+            <span className="duration"></span>
+            </div>
+            <div className="train-footer">
+                <button onClick={toggleStations} className="stopping-stations-btn">
+                    Stopping Stations
+                </button>
+                <div className="last-update-box">
+                    <div className="last-update">
+                    <span className="update-label">Last Update:</span>
+                    <span className="update-value">{train.lastUpdateStation}</span>
+                    <span className="update-value">{train.lastUpdateTime}</span>
+                    </div>
+                </div>
+            </div>
+            {isExpanded && (
+            <div className="stopping-stations">
+                <ul>
+                {train.stoppingStations.map((station, index) => (
+                    <li key={index}>{station}</li>
+                ))}
+                </ul>
+            </div>
+            )}
+        </div>
     </div>
   );
 };
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import '../styles/home.css';
 import TrainCard from '../components/trainCard.js';
 
 const Home = () => {
+
+  const trains = [
+    {
+      route: 'Batticaloa - Colombo Fort',
+      trainNo: '6012',
+      trainName:'Udaya Devi',
+      type: 'Express',
+      days: 'Mon, Tue, Wed, Thu, Fri, Sat, Sun, Poya, Holi',
+      startTime: '6:10:00',
+      endTime: '3:05:00',
+      startStation: 'Batticaloa',
+      endStation: 'Colombo Fort',
+      duration: '8 h 55 m',
+      lastUpdateStation: 'Konwewa',
+      lastUpdateTime: '12:33 PM',
+      stoppingStations: ['Station 1', 'Station 2', 'Station 3', 'Station 4'],
+    },
+  ];
+
   return (
     <div>
-      <h1>Home Page</h1>
-      <TrainCard />
+      <div className="container train-card-container">
+        {trains.map((train, index) => (
+          <TrainCard key={index} train={train} />
+        ))}
+      </div>
     </div>
   );
 };
