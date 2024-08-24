71ebb7dc272948d48dd9821bd9a408d31817881a
nimansu-fernando <nimansufernando@gmail.com>
2024-08-25 03:03:52 +0530

    Add model to show the map and neccessary CSS changes

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import '../styles/home.css';
 import TrainCard from '../components/trainCard.js';
+import Modal from 'react-bootstrap/Modal';
+import Map from '../components/map.js';
+import Header from '../components/header.js'; 
+import locationIcon from '../assets/location.png';
 
 const Home = () => {
   const [trains, setTrains] = useState([]);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
   useEffect(() => {
     const fetchTrainData = async () => {
@@ -23,13 +28,34 @@ const Home = () => {
     fetchTrainData();
   }, []);
 
+  const openModal = () => setModalIsOpen(true);
+  const closeModal = () => setModalIsOpen(false);
+
   return (
     <div className="home-page">
+      <Header />
+      <button onClick={openModal} className="live-map-button">
+        <img src={locationIcon} alt="Live Icon" className="icon-image" />
+        Watch Live Map
+      </button>
       <div className="container train-card-container">
         {trains.map((train, index) => (
           <TrainCard key={index} train={train} />
         ))}
       </div>
+      <Modal show={modalIsOpen} onHide={closeModal} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Current Train Locations</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Map trains={trains} />
+        </Modal.Body>
+        <Modal.Footer>
+          <button onClick={closeModal} className="btn btn-secondary">
+            Close
+          </button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
